71236364e9b993c058e05a1f8f1ed9f7d3b189a7
Enoch Kim <enochkim91@gmail.com>
2017-06-15 12:14:42 -0700

    Initial set up for asyncMap

---
+++ hermonies-handbag/asyncMap.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/* Implement the function asyncMap:
+ *
+ * asyncMap has two parameters, an array of asynchronous functions (tasks) and a callback.
+ * Each of the tasks takes a separate callback and invokes that callback when complete.
+ *
+ * The callback passed to asyncMap is then performed on the results of the callbacks of the tasks.
+ *
+ * The order of these results should be the same as the order of the tasks.
+ * It is important to note that this is not the order in which the tasks return,
+ * but the order in which they are passed to asyncMap.
+ *
+ * Once all the callbacks of the tasks are returned, asyncMap should invoke the callback
+ * on the results array.
+ *
+ *
+ * Example:
+ *
+ * asyncMap([
+ *  function(cb){
+ *    setTimeout(function(){
+ *      cb('one');
+ *    }, 200);
+ *  },
+ *  function(cb){
+ *    setTimeout(function(){
+ *      cb('two');
+ *    }, 100);
+ *  }
+ * ],
+ *  function(results){
+ *    // the results array will equal ['one','two'] even though
+ *    // the second function had a shorter timeout.
+ *    console.log(results); // ['one', 'two']
+ * });
+ *
+ *
+ */
+
+
+var asyncMap = function(tasks, callback) {
+  // we have to invoke the first function in the array
+    //wait until we get a response before moving on to the next function - async
+  // what is the callback doing? - logging out the array at the end of the map
+  // can we use promises to keep this async..?  
+
+
+  // start with a new promise function that loops through the tasks,
+    //promisify the first task in the array and invoke,
+    // chain it to the next task
+  // when the loop is down chain task to the callback?
+};

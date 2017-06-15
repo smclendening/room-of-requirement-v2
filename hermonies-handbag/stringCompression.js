4b338dead9f9933388d51601d785d47a4d359f42
Enoch Kim <enochkim91@gmail.com>
2017-06-14 21:47:54 -0700

    Updated file structure - set up initial file for string compression problem

---
+++ hermonies-handbag/stringCompression.js
@@ -0,0 +1,11 @@
+/*
+Given a string, return a compressed string that has the respective letter 
+and number of repeated characters.
+
+Example:
+
+stringCompress('aaaffskkaaggfg') -> 'a3f2s1k2a2g2f1g1'
+stringCompress('Dolby') -> 'D1o1l1b1y1'
+*/
+
+

b1b895dd46d6ac5568e709903c00ba0da2a3e477
Enoch Kim <enochkim91@gmail.com>
2017-06-28 22:12:53 -0700

    First attempt, have a bug in first iteration through

--- hermonies-handbag/stringCompression.js
+++ hermonies-handbag/stringCompression.js
@@ -9,7 +9,24 @@ stringCompress('Dolby') -> 'D1o1l1b1y1'
 */
 
 let stringCompress = (string) => {
+  let result = '';
+  let currentLetter = string[0]
+  let currentCount = 0;
+  for(let i = 0; i < string.length; i++) {
+    console.log(currentLetter, string[i]);
+    if (currentLetter === string[i]) {
+      currentCount++;
+    } else {
+      result += ('' + currentLetter + currentCount);
+      currentLetter = string[i + 1] || '';
+      console.log(currentLetter);
+      currentCount = 0;
+    }
+  }
 
+  return result;
 }
+
+console.log(stringCompress('asd'));
 
 

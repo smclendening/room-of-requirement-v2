14c836828c71885fc0d99bfc09721e3a4e02956e
Enoch Kim <enochkim91@gmail.com>
2017-07-03 16:21:10 -0700

    Throttle file started, debounce - handled third parameter to handle if it needs tp be immediate

--- hermonies-handbag/debounce.js
+++ hermonies-handbag/debounce.js
@@ -1,6 +1,6 @@
 //Debounce from scratch;
 
-let debounce = (func, wait) => {
+let debounce = (func, wait, immediate) => {
   var timeout;
   return () => {
     var context = this;
@@ -9,8 +9,30 @@ let debounce = (func, wait) => {
     //Clear current timeout sessions
     clearTimeout(timeout);
     timeout = setTimeout(function() {
-      func.apply(ctx, args);
+      timeout = null;
+      if(!immediate) {
+        func.apply(context, args);
+      }
     }, wait);
+    let callNow = immediate && !timeout;
+    if (callNow) {
+      func.apply(context, args);
+    }
   };
 
 };
+
+let hello = () => {
+  console.log('Hello');
+}
+
+let sayHelloOnce = debounce(hello, 100);
+
+
+sayHelloOnce();
+sayHelloOnce();
+sayHelloOnce();
+sayHelloOnce();
+sayHelloOnce();
+sayHelloOnce();
+sayHelloOnce();
